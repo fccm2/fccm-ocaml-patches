30c92f3b57af676d723df82c883f525a6c9a30e4
Florent Monnier <monnier.florent@gmail.com>
2026-08-10 21:25:02 +0200

    a new preprocessor

---
+++ patch/mslpp/built-1.sh
@@ -0,0 +1,77 @@
+wget https://fccm2.github.io/fccm-ocaml-download/download/downloads-04/mgkmsl/mgk-gen-msl-0.01.5b.tar.lzma
+
+lzcat mgk-gen-msl-0.01.5b.tar.lzma | tar xf -
+
+
+wget https://fccm2.github.io/fccm-ocaml-download/download/downloads-04/mgkmsl/mgk-gen-msl-0.01.5b-c.patch.lzma
+wget https://fccm2.github.io/fccm-ocaml-download/download/downloads-04/mgkmsl/mgk-gen-msl-0.01.5c-d.patch.lzma
+wget https://fccm2.github.io/fccm-ocaml-download/download/downloads-04/mgkmsl/mgk-gen-msl-0.01.5d-e.patch.lzma
+wget https://fccm2.github.io/fccm-ocaml-download/download/downloads-04/mgkmsl/mgk-gen-msl-0.01.5e-f.patch.lzma
+
+unlzma mgk-gen-msl-0.01.5b-c.patch.lzma
+unlzma mgk-gen-msl-0.01.5c-d.patch.lzma
+unlzma mgk-gen-msl-0.01.5d-e.patch.lzma
+unlzma mgk-gen-msl-0.01.5e-f.patch.lzma
+
+
+cd mgk-gen-msl-0.01.5b/
+
+patch -p1 < ../mgk-gen-msl-0.01.5b-c.patch
+patch -p1 < ../mgk-gen-msl-0.01.5c-d.patch
+patch -p1 < ../mgk-gen-msl-0.01.5d-e.patch
+patch -p1 < ../mgk-gen-msl-0.01.5e-f.patch
+
+cd ..
+mv mgk-gen-msl-0.01.5b/ mgk-gen-msl-0.01.5f/
+
+
+wget https://fccm2.github.io/fccm-ocaml-patches/patch/mslpp/mgk-gen-msl-0.01.5f-7a.patch.lzma
+wget https://fccm2.github.io/fccm-ocaml-patches/patch/mslpp/mgk-gen-msl-0.01.7a-7b.patch.lzma
+
+unlzma mgk-gen-msl-0.01.5f-7a.patch.lzma
+unlzma mgk-gen-msl-0.01.7a-7b.patch.lzma
+
+cd mgk-gen-msl-0.01.5f/
+
+patch -p1 < ../mgk-gen-msl-0.01.5f-7a.patch
+patch -p1 < ../mgk-gen-msl-0.01.7a-7b.patch
+
+cd ..
+
+mv mgk-gen-msl-0.01.5f/ mgk-gen-msl-0.01.7b/
+
+wget https://fccm2.github.io/fccm-ocaml-patches/patch/mslpp/mgk-gen-msl-0.01.7b-7c.patch.lzma
+wget https://fccm2.github.io/fccm-ocaml-patches/patch/mslpp/mgk-gen-msl-0.01.7c-7d.patch.lzma
+wget https://fccm2.github.io/fccm-ocaml-patches/patch/mslpp/mgk-gen-msl-0.01.7d-7e.patch.lzma
+
+unlzma mgk-gen-msl-0.01.7b-7c.patch.lzma
+unlzma mgk-gen-msl-0.01.7c-7d.patch.lzma
+unlzma mgk-gen-msl-0.01.7d-7e.patch.lzma
+
+cd mgk-gen-msl-0.01.7b/
+
+patch -p1 < ../mgk-gen-msl-0.01.7b-7c.patch
+patch -p1 < ../mgk-gen-msl-0.01.7c-7d.patch
+patch -p1 < ../mgk-gen-msl-0.01.7d-7e.patch
+
+mv mgk-gen-msl-0.01.5b/ mgk-gen-msl-0.01.7e/
+
+cd src/
+make
+cd ../path/
+make
+
+cd ..
+cd msl/
+
+make dep
+make
+make opt
+
+#man ./xmgk.1
+
+#make -f strings.mk opt
+
+make -f pppo.mk opt
+make -f pppo7o.mk opt
+
